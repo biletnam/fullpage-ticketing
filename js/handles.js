31547c670a29192e4cfbec65a917a2a6ee30eed2
Philipp Heller <pheller1999@gmail.com>
2017-10-13 09:28:44 +0200

    changed post structure

--- js/handles.js
+++ js/handles.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
   */
   $("#createReviewPage").click(function() {
     $("#reviewTable").empty();
-    $(".ticketOrder").each(function () {
-      $("#reviewTable").prepend("<tr><td class='reviewName'>"+$(this).val()+"<br>Early-Bird Ticket</td><td class='reviewPrice'>€20.00</td></tr>");
+    $(".ticketOrder").each(function () {                                                                        /*hardcoded for 2 digit prices*/
+      $("#reviewTable").prepend("<tr><td class='reviewName'>"+$(this).val()+"<br>Early-Bird Ticket</td><td class='reviewPrice'>€20</td></tr>");
     });
   });
 
@@ -28,15 +28,16 @@ $(document).ready(function() {
   var tickets = 1;
   $("#addticketbutton").click(function() {
     tickets++;
-    $(this).before('<input type="text" class="wide ticketOrder" id="name-ticket'+ tickets +'" name="tickets[]" placeholder="z.B. Max Mustermann"><button type="button" class="removeTicketButton">-</button><br>');
+    $(this).before('<input type="text" class="wide ticketOrder" id="name-ticket'+ tickets +'" name="tickets[]" placeholder="z.B. Max Mustermann"><input type="hidden" name="tickets[uids][]" value="88fasodfho72as7asd2asdoier"><button type="button" class="removeTicketButton">-</button><br>');
   });
 
   /** Button to remove a ticket
   */
   $("#step2").on("click", ".removeTicketButton", function() {
     tickets--;
-    $(this).prev().remove();
-    $(this).next().remove();
-    $(this).remove();
+    $(this).prev().remove(); //owner
+    $(this).prev().remove(); //uid
+    $(this).next().remove(); //br
+    $(this).remove(); //this
   });
 });
