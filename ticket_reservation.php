931dd36f9bbfc77517815da5d871f787939fa65c
Philipp Heller <pheller1999@gmail.com>
2017-10-13 20:02:09 +0200

    fixed reservation check query

--- ticket_reservation.php
+++ ticket_reservation.php
@@ -1,14 +1,14 @@
 <?php
   include "db.php";
 
-  //$sql = "SELECT stage,price FROM pricing WHERE left <> 0 LIMIT 1";
-  $sql = "SELECT * FROM pricing WHERE left = '60'";
+  $sql = "SELECT pricing.stage,pricing.price FROM pricing WHERE pricing.left <> 0 LIMIT 1";
+  //$sql = "SELECT * FROM pricing WHERE pricing.left = '60'";
 
   if(!$result = $db->query($sql)){
     die('There was an error running the query [' . $db->error . ']');
   }
 
   while($row = $result->fetch_assoc()){
-    echo $row['stage'].$row['price'].'<br />';
+    echo $row['stage']."-€".$row['price'].'<br />';
   }
  ?>
