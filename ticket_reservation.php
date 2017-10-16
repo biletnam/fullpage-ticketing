7b687b21bc9f1877a9d182b85b4f4e8e246c166b
Philipp Heller <pheller1999@gmail.com>
2017-10-16 12:53:06 +0200

    finished query to select cheapest avaible ticket

--- ticket_reservation.php
+++ ticket_reservation.php
@@ -1,8 +1,7 @@
 <?php
   include "db.php";
 
-  $sql = "SELECT pricing.stage,pricing.price FROM pricing WHERE pricing.left <> 0 LIMIT 1";
-  //$sql = "SELECT * FROM pricing WHERE pricing.left = '60'";
+  $sql = "SELECT pricing.stage,pricing.price FROM pricing WHERE (expiration >= NOW()) AND ((pricing.left > 0) OR (pricing.left IS NULL)) ORDER BY pricing.priority LIMIT 1";
 
   if(!$result = $db->query($sql)){
     die('There was an error running the query [' . $db->error . ']');
@@ -10,5 +9,11 @@
 
   while($row = $result->fetch_assoc()){
     echo $row['stage']."-€".$row['price'].'<br />';
+  }
+
+  if($_SERVER['REQUEST_METHOD'] ==  "GET") {
+    if(isset($_GET['reserve'])) {
+      echo "true";
+    }
   }
  ?>
