64275a3c3741fa2b632b45c7f7369a1ce8538547
Philipp Heller <pheller1999@gmail.com>
2017-10-16 14:54:37 +0200

    implemented reservation process
    only for the backend yet, about to be added to frontend

--- ticket_reservation.php
+++ ticket_reservation.php
@@ -1,19 +1,31 @@
 <?php
   include "db.php";
 
-  $sql = "SELECT pricing.stage,pricing.price FROM pricing WHERE (expiration >= NOW()) AND ((pricing.left > 0) OR (pricing.left IS NULL)) ORDER BY pricing.priority LIMIT 1";
-
-  if(!$result = $db->query($sql)){
-    die('There was an error running the query [' . $db->error . ']');
-  }
-
-  while($row = $result->fetch_assoc()){
-    echo $row['stage']."-€".$row['price'].'<br />';
-  }
-
   if($_SERVER['REQUEST_METHOD'] ==  "GET") {
-    if(isset($_GET['reserve'])) {
-      echo "true";
+    if(isset($_GET['cart'])) {
+      //////get cheapest possible price
+      $sql = "SELECT pricing.stage,pricing.price FROM pricing WHERE (expiration >= NOW()) AND ((pricing.left > 0) OR (pricing.left IS NULL)) ORDER BY pricing.priority LIMIT 1";
+      $row = $db->query($sql)->fetch_assoc();
+      //echo $row['stage']."-€".$row['price'];
+
+      //////decrease number of avaible tickets
+      $sql = "UPDATE pricing SET pricing.left = pricing.left - 1 WHERE stage = ".$row['stage']." AND stage > 0";
+
+      if(!$result = $db->query($sql)){
+        die('There was an error running the update query [' . $db->error . ']');
+      }
+      //////add ticket to cart
+      ////generate uid
+      $uid = md5($row['stage'].$row['price'].time()).$row['price'];
+
+      $sql = "INSERT INTO carted (uid, price) VALUES ('".$uid."', '".$row['price']."')";
+      if(!$result = $db->query($sql)){
+        die('There was an error running the insert query [' . $db->error . ']');
+      }
+
+      //todo: return uid and price as JSON
+      echo "{\"uid\":\"".$uid."\", \"price\":".$row['price']."}";
+
     }
   }
  ?>
