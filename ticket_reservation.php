ee6f7d23db987f82a3d66af2af4ec7feeca70b6f
Philipp Heller <pheller1999@gmail.com>
2017-10-16 19:07:50 +0200

    fixed ticket reavailability
    ticket was removed from carted, but not readded to supply

--- ticket_reservation.php
+++ ticket_reservation.php
@@ -4,12 +4,12 @@
   if($_SERVER['REQUEST_METHOD'] ==  "GET") {
     if(isset($_GET['cart'])) {
       //////get cheapest possible price
-      $sql = "SELECT pricing.stage,pricing.name,pricing.price FROM pricing WHERE (expiration >= NOW()) AND ((pricing.left > 0) OR (pricing.left IS NULL)) ORDER BY pricing.priority LIMIT 1";
+      $sql = "SELECT pricing.stage,pricing.name,pricing.price FROM pricing WHERE (pricing.expiration >= NOW()) AND ((pricing.left > 0) OR (pricing.left IS NULL)) ORDER BY pricing.priority LIMIT 1";
       $row = $db->query($sql)->fetch_assoc();
       //echo $row['stage']."-€".$row['price'];
 
       //////decrease number of avaible tickets
-      $sql = "UPDATE pricing SET pricing.left = pricing.left - 1 WHERE stage = ".$row['stage']." AND stage > 0";
+      $sql = "UPDATE pricing SET pricing.left = pricing.left - 1 WHERE pricing.stage = ".$row['stage']." AND stage > 0";
 
       if(!$result = $db->query($sql)){
         die('There was an error running the update query [' . $db->error . ']');
@@ -36,7 +36,7 @@
         $success = false;
       }
 
-      $result->fetch_assoc();
+      $row = $result->fetch_assoc();
 
       //////delete reservation
       $sql = "DELETE FROM carted WHERE carted.uid = '".$uid."'";
@@ -46,13 +46,13 @@
       }
 
       //////make this ticket reavaible
-      $sql = "UPDATE pricing SET pricing.left = pricing.left + 1 WHERE stage = ".$row['stage'];
+      $sql = "UPDATE pricing SET pricing.left = pricing.left + 1 WHERE pricing.stage = ".$row['stage'];
 
       if(!$result = $db->query($sql)){
         $success = false;
       }
 
-      echo "{\"success\":\"".$success."\"}";
+      echo "{\"success\":\"".($success?"true":"false")."\"}";
 
     }
   }
