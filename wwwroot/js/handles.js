2220126e5ca1138330c134059c45965781a4d6c3
Philipp Heller <pheller1999@gmail.com>
2017-10-19 19:44:18 +0200

    removed ENTER submit
    because this would skip pages

--- wwwroot/js/handles.js
+++ wwwroot/js/handles.js
@@ -91,3 +91,11 @@ $(document).ready(function() {
     });
   });
 });
+
+$(document).on("keypress", 'form', function (e) {
+    var code = e.keyCode || e.which;
+    if (code == 13) {
+        e.preventDefault();
+        return false;
+    }
+});
